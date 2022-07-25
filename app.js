dff91bb5381d26724372c8db60aa20f46ec7a635
Vier <taurustuhoang@gmail.com>
2022-07-25 09:04:24 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typing-1', {
+    strings: ['Freelancer', 'Designer', 'Gamer'],
+    smartBackspace: true,
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity // Default value
+});
