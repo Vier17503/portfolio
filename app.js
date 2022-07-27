f89201705dff047f76735cdf42b0ad1a4769fc85
Vier <taurustuhoang@gmail.com>
2022-07-28 04:14:38 +0700

    big update

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var typed = new Typed('.typing-1', {
     loop: true,
     loopCount: Infinity // Default value
 });
+var typed = new Typed('.typing-2', {
+    strings: ['Freelancer', 'Designer', ],
+    smartBackspace: true,
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true,
+    loopCount: Infinity // Default value
+});
 
 let text = document.getElementById('text');
 window.addEventListener('scroll', function() {
