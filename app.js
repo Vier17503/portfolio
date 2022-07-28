630efd5fecca81df4400ee186f02ae6dc910ea76
Vier <taurustuhoang@gmail.com>
2022-07-28 13:35:02 +0700

    ?

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// $(document).ready(function() {
+//     setTimeout(function() {
+//         $('.wrapper').addClass('loaded');
+
+//     }, 3000);
+// });
+
+
+
+// jQuery(function() {
+
+//     $(window).load(function() {
+
+//         $('.wrapper').removeClass('preload');
+
+//     });
+
+// });
+
 var typed = new Typed('.typing-1', {
     strings: ['Freelancer', 'Designer', ],
     smartBackspace: true,
@@ -19,21 +38,4 @@ let text = document.getElementById('text');
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
     text.style.transform = `translateX(-${value*3}px)`
-});
-
-$('.iconM-testimonials').on('click', function() {
-    $("#colorscreen").removeClass("fadeInUpBig").addClass("fadeInUpBig");
-    $('.fadeInUpBig').css('background-color', 'rgba(164, 196, 0, 0.2)');
-    $(".tile-group.main").css({ marginLeft: "-40px", width: "1080px" }).load("musability-musictherapy-company-overview.html");
-
-
-});
-
-
-$('.iconM-referrals').on('click', function() {
-    $("#colorscreen").removeClass("fadeInUpBig").addClass("fadeInUpBig");
-    $('.fadeInUpBig').css('background-color', 'rgba(183, 192, 26, 0.8)');
-    $(".tile-group.main").css({ marginLeft: "-40px", width: "1080px" }).load("musability-musictherapy-company-overview.html");
-
-
 });
